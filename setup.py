a6e1d89d8227f3cb58a251c52d497d1aac627953
Jan Gieseler <jan.gieseler@hotmail.com>
2016-10-27 11:16:36 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Pyoommf',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Jan Gieseler',
+    author_email='',
+    description=''
+)
